81794eb34be8a501f0a2c74138e16b712ade592f
Hom Yan <yanzhihong23@qq.com>
2016-01-27 19:11:32 +0800

    toggle aside nav

--- src/app/components/login-info/login.info.directive.js
+++ src/app/components/login-info/login.info.directive.js
@@ -6,13 +6,15 @@
     .directive('loginInfo', loginInfo);
 
   /** @ngInject */
-  function loginInfo() {
+  function loginInfo($rootScope) {
     var directive = {
       restrict: 'E',
       scope: true,
       templateUrl: 'app/components/login-info/login.info.html',
       link: function(scope, element, attr) {
-        
+        scope.toggle = function() {
+          $rootScope.hideAside = !$rootScope.hideAside;
+        }
       }
     };
 
